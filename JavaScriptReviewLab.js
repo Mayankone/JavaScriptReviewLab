d1dfba8efc4ba51dd00736f6300c7dd7b336e91b
Mayank Kumar <computergeekone@gmail.com>
2022-12-01 13:51:04 -0500

    first commit

--- JavaScriptReviewLab.js
+++ JavaScriptReviewLab.js
@@ -13,4 +13,33 @@ let secondVariable = firstVariable; //B4
 secondVariable = "Second String"; //B5
 console.log(firstVariable); // The value of the first var is 10. B6
 let yourName = "Mayank Kumar";
-console.log("Hello my name is " + yourName);
+console.log("Hello my name is " + yourName); //B7
+
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+
+console.log(a < b);
+console.log(c>d);
+console.log('Name' ==='Name');
+// FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log(false && false && false && false && false || true);
+console.log(false == false)
+console.log(e == 'Kevin');
+console.log(a < b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a != a + d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 == '48');
+// C Booleans
+
+let animal = "cow";
+if(animal == "cow"){
+    console.log("MOOOOOOOOOOO")
+}
+else {
+    console.log("You're not a cow!");
+}
